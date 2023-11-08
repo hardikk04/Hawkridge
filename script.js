5cc49d2ae5c409db92a2ce7696c1fa52f8af4513
hardikk04 <hardik04062003@gmail.com>
2023-11-08 09:49:47 +0530

    done

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector(".main"),
   smooth: true,
 });
+
+gsap.to(".loder", {
+  top: "-100vh",
+  duration: 1,
+  delay: 1,
+  ease: "power3.inOut",
+});
